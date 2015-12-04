73f3222385e5f4da692e20fc5085374dae11c78a
Bartosz Nowak <sztosz@gmail.com>
2015-12-04 20:08:20 +0100

    Added methods body to student decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,10 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    if subject_item_notes.find_by(subject_item: subject_item)
+      '%.2f' % subject_item_notes.where(subject_item: subject_item).average(:value)
+    else
+      '0.00'
+    end
   end
 end
