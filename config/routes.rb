7f3a1bd82d24a72bc650c573d9f598fb321e63c3
Bartosz Nowak <sztosz@gmail.com>
2015-12-04 19:20:18 +0100

    Adding devise, teachers and reports to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'visitors#index'
   resources :students do
     get :subjects
   end
+
+  resource :teachers
+
+  resource :reports do
+    get :subjects
+  end
+
+  devise_for :user
 end
